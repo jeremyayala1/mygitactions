038aaa51f003ec5e0c16b16342aa9dbe11bd8295
jeremyayala1 <124664483+jeremyayala1@users.noreply.github.com>
2024-11-11 21:24:18 -0800

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,7 +1,6 @@
 def calculate_sum(a, b):
     total = a + b
-    return total  # Removed unused variable
-
+    return total
 
 result = calculate_sum(3, 4)
-print(result)  # Now 'result' is used
+print(result)
