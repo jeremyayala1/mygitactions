4b43ab84c22426671567104cf56601d7630a4374
jeremyayala1 <124664483+jeremyayala1@users.noreply.github.com>
2024-11-11 19:47:47 -0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,8 @@
 import math
 
+
 def calculate_area(radius):
-    return math.pi * radius ** 2  # Use math.pi correctly
+    return math.pi * radius**2  # Use math.pi correctly
+
 
 result = calculate_area(5)
